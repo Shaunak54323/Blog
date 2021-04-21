78afcd47497ae36a1d58a312935b42c04d40d41a
Shaunak54323 <bagadeshaunak@gmail.com>
2021-04-21 09:06:11 +0530

    21/4/2021 commit

---
+++ articles/views.py
@@ -0,0 +1,32 @@
+from django.views.generic import ListView
+from django.views.generic.edit import UpdateView, DeleteView, CreateView
+from .models import *
+from django.urls import reverse_lazy
+
+
+class ArticleListView(ListView):
+    model = Article
+    template_name = 'article_list.html'
+
+
+class ArticleDetailView(DetailView):
+    model = Article
+    template_name = 'article_detail.html'
+
+
+class ArticleUpdateView(UpdateView):
+    model = Article
+    fields = ('title', 'body',)
+    template_name = 'article_edit.html'
+
+
+class ArticleDeleteView(DeleteView):
+    model = Article
+    template_name = 'article_delete.html'
+    success_url = reverse_lazy('article_list')
+
+
+class ArticleCreateView(CreateView):
+    model = Article
+    template_name = 'article_new.html'
+    fields = ('title', 'body', 'author',)
--- newspaper_project/settings.py
+++ newspaper_project/settings.py
@@ -34,6 +34,7 @@ INSTALLED_APPS = [
     # Local
     'users',
     'pages',
+    'articles',
 
     # 3rd Party
     'crispy_forms',
@@ -113,7 +114,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/New_York'
 
 USE_I18N = True
 
