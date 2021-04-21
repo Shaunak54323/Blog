d3ba08b2a6094b973282523613f87715e4ce9c34
Shaunak54323 <bagadeshaunak@gmail.com>
2021-04-21 09:45:48 +0530

    21/4/2021 final commit

--- articles/admin.py
+++ articles/admin.py
@@ -1,4 +1,16 @@
 from django.contrib import admin
-from .models import Article
+from .models import Article, Comment
+
+
+class CommentInline(admin.TabularInline):
+    model = Comment
+
+
+class ArticleAdmin(admin.ModelAdmin):
+    inlines = [
+        CommentInline,
+    ]
+
 
 admin.site.register(Article)
+admin.site.register(Comment)
